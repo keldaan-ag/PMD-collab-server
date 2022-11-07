62b1fb151becb65b1f673b77ada98f12f40eee77
keldaan <arnaudgregoire92@gmail.com>
2022-11-07 20:46:14 +0100

    small css

--- src/components/DungeonThumbnail.tsx
+++ src/components/DungeonThumbnail.tsx
@@ -4,8 +4,13 @@ export function DungeonThumbnail(props:{dungeon:Dungeon}){
     return <div className='nes-container nes-pointer' style={{
         margin:'10px',
          padding:'10px',
-         maxWidth:'174px',
-         backgroundColor:'white'
+         maxWidth:'160px',
+         backgroundColor:'white',
+         display:'flex',
+         flexFlow:'column',
+         alignItems:'center',
+         justifyContent:'space-between',
+         border:'2px solid black'
          }}>
         <img key={props.dungeon} src={process.env.PUBLIC_URL + '/preview/' + props.dungeon + '-preview.png'} alt={props.dungeon}/>
         <p style={{fontSize:'0.55em'}}>{DungeonData[props.dungeon].name}</p>
