f995227b3cbd957a0a9330fde118bafc16ac9ff9
keldaan <arnaudgregoire92@gmail.com>
2022-11-09 11:01:35 +0100

    parameters

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -7,6 +7,8 @@ export function Sample(props: { dungeon: Dungeon }) {
   const data = DungeonData[props.dungeon]
   const [imageData, setImageData] = useState<ImageData | undefined>()
   const [img, setImg] = useState<HTMLImageElement | undefined>()
+  const [persistance, setPersistance] = useState<number>(0.1)
+  const [frequency, setFrequency] = useState<number>(5)
 
   useEffect(() => {
     const virtualCanvas = document.createElement("canvas")
@@ -34,11 +36,18 @@ export function Sample(props: { dungeon: Dungeon }) {
 
   useEffect(() => {
     generate()
-  }, [imageData])
+  }, [imageData, persistance, frequency])
 
   function generate() {
     if (imageData) {
-      const design = new Design(props.dungeon, imageData, 5, 0.1, 54, 26)
+      const design = new Design(
+        props.dungeon,
+        imageData,
+        frequency,
+        persistance,
+        54,
+        28
+      )
       const canvas = document.getElementById(`sample`) as HTMLCanvasElement
 
       if (canvas && img) {
@@ -73,9 +82,33 @@ export function Sample(props: { dungeon: Dungeon }) {
         <button className="nes-btn is-primary" onClick={generate}>
           Generate
         </button>
+        <p>Persistence</p>
+        <input
+          min={0.01}
+          max={0.99}
+          step={0.01}
+          type={"range"}
+          value={persistance}
+          onChange={(e) => {
+            setPersistance(parseFloat(e.target.value))
+          }}
+        />
+        <p>{persistance}</p>
+        <p>Frequency</p>
+        <input
+          min={0.1}
+          max={10}
+          step={0.1}
+          type={"range"}
+          value={frequency}
+          onChange={(e) => {
+            setFrequency(parseFloat(e.target.value))
+          }}
+        />
+        <p>{frequency}</p>
       </div>
       <div className="nes-container" style={{ padding: "0px" }}>
-        <canvas width={1350} height={600} id="sample"></canvas>
+        <canvas width={1350} height={650} id="sample"></canvas>
       </div>
     </div>
   )
